5c00426a3491d374e1bb1515a79c8382d2669223
funasty1985 <tony.f.liang@gmail.com>
2020-04-10 15:34:20 +0800

    40. Project setup pt2

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb+srv://fu_liang:fu2017fu@fu-h0vum.mongodb.net/blog_dev?retryWrites=true&w=majority',
   cookieKey: '123123123'
 };

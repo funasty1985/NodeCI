aa37472d14068c5d9f9263fe11cf7be2d302d16f
funasty1985 <tony.f.liang@gmail.com>
2020-04-25 19:08:27 +0800

    41-51 Data Caching with Redis pt1
    - basic setup for redis
    - caching targeted /api/blog
    - Issue needed to fix
          1. cached key pair remain unchange , even if the blogs are updated
          2. codes for cached is not reusable
          3. req.user.id as the caching redis key making the caching only limited to one resource(ie blog post)

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+    '927031463508-3omnb7hqdifartorro288h9l4qrrq108.apps.googleusercontent.com',
+  googleClientSecret: 'Cw6kiZP8P3uygqqq8mqvW-qD',
   mongoURI: 'mongodb+srv://fu_liang:fu2017fu@fu-h0vum.mongodb.net/blog_dev?retryWrites=true&w=majority',
   cookieKey: '123123123'
 };
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,9 +14,40 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
+    const redis = require('redis');
+    const redisUrl = 'redis://172.17.0.2';
+    const client = redis.createClient(redisUrl);
+    const util = require('util');
+
+    // client.get from node-redis doesn't return a promise object, 
+    // but instead accepting a callback to handle its returned result
+    // e.g client.get('key',(r)=>{console.log(r)}) 
+    // promisify will overwrite client.get and make it return a promise object
+    // so that the new client.get is easier to read and write. 
+    client.get = util.promisify(client.get); 
+
+    // Do we have any cached data in redis related 
+    // to this query 
+
+    const cacheBlogs = await client.get( req.user.id );
+
+    // if yes, then respond to the request right away 
+    // and return
+
+    if (cacheBlogs) {
+      console.log('SERVER FROM CACHE')
+      return res.send(JSON.parse(cacheBlogs)) // JSON -> js object
+    }
+
+    // if no, we need to respond to request 
+    // and updata our cach to store the data
+
+    console.log('SERVER FROM MONGODB')
     const blogs = await Blog.find({ _user: req.user.id });
 
     res.send(blogs);
+
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {

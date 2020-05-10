106ecb3b33b7557d16d23e6278638a121843c5b1
funasty1985 <tony.f.liang@gmail.com>
2020-05-10 11:31:28 +0800

    force git add ci.js

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID:
+      '927031463508-3omnb7hqdifartorro288h9l4qrrq108.apps.googleusercontent.com',
+    googleClientSecret: 'Cw6kiZP8P3uygqqq8mqvW-qD',
+    mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+    cookieKey: '123123123',
+    redisUrl: 'redis://127.0.0.1:6379'
+  };
